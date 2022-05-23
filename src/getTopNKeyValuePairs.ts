ff78563c0a572fe4eb79da835f305b77bc2f2cb4
Mitchell (Midly) Roberts <mitchell.roberts11@gmail.com>
2022-05-23 21:54:32 +1000

    refactor: nicen output

--- src/getTopNKeyValuePairs.ts
+++ src/getTopNKeyValuePairs.ts
@@ -1,7 +1,7 @@
 const getTopNKeyValuePairs = (object: { [key: string]: number }, n: number) => {
-    const keyValuePairArray = Object.entries({ ...object }).map(([key, value]) => ({ keyString: key, valueInt: value }));
+    const keyValuePairArray = Object.entries({ ...object }).map(([key, value]) => ({ keyString: key, count: value }));
     const sortedPairs = keyValuePairArray.sort((a, b) => {
-        return b.valueInt - a.valueInt
+        return b.count - a.count
     });
     return sortedPairs.slice(0, n);
 }
